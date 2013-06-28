7aad85722e0ecd28e3fb2030d26c07d1a3f0aff4
Tuomas Mäkelä <tuomas.makela@kapsi.fi>
2013-06-28 19:06:11 +0300

    Initial version of the photo folder renamer.

---
+++ renamer.rb
@@ -0,0 +1,15 @@
+rootDirectory = "/Users/tumakela/Pictures/Raaka/"
+
+Dir.foreach(rootDirectory) do |item|
+	schemas =
+		[/[0-9]{8}/] #yyyymmdd
+	next item if item.start_with?(".")
+	schemas.each do |schema|
+		if schema.match(item)
+			oldName = rootDirectory + item
+			newName = rootDirectory + item[0, 4] + "-" + item[4, 2] + "-" + item[6, 2]
+			puts "Renaming " + oldName + " to " + newName
+			File.rename oldName, newName
+		end
+	end
+end
